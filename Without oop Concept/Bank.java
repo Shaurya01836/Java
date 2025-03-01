a25a5925a1b36644701cb3805aa005d836536f7f
Shaurya Upadhyay <shaurya01836@gmail.com>
2025-03-01 15:16:50 +0530

    updating code

--- Without oop Concept/Bank.java
+++ Without oop Concept/Bank.java
@@ -2,19 +2,24 @@ import java.util.Scanner;
 
 class Bank {
 	public static void main(String[] args) {
-		float balance = 0, depositAmount = 0, withdrawAmount = 0;
+		double balance = 0, depositAmount = 0, withdrawAmount = 0;
 		int choice;
+		String PhoneNo = "";
 		Scanner s = new Scanner(System.in);
-		System.out.print("Are you a New Customer if yes(1)or no(0):");
+		System.out.print("Are you a New Customer if yes(1) or no(0):");
 		choice = s.nextInt();
 		if (choice == 1) {
 			System.out.println("We can create a new account for you");
 			System.out.print("Do you want to deposit any amount if yes(1) or no(0):");
 			choice = s.nextInt();
 			if (choice == 1) {
-				System.out.print("Enter the amount you want to deposit :");
-				depositAmount = s.nextInt();
-				balance += depositAmount;
+				System.out.print("Enter the deposit amount: ");
+				depositAmount = s.nextDouble();
+				if (depositAmount >= 0) {
+					balance += depositAmount;
+				} else {
+					System.out.println("Invalid deposit amount. Initial balance set to 0.");
+				}
 			} else {
 				System.out.print("We have sucessfully created your account with 0 balance");
 			}
@@ -24,27 +29,44 @@ class Bank {
 			System.out.println("1. Deposit Money");
 			System.out.println("2. Withdraw Money");
 			System.out.println("3. Check Balance");
-			System.out.println("4. Exit");
+			System.out.println("4. Check Your Phone no.");
+			System.out.println("5. Change Your Phone no.");
+			System.out.println("6. Exit");
 			System.out.print("Enter your choice :");
 			choice = s.nextInt();
 			switch (choice) {
 				case 1:
 					System.out.print("Enter the amount you want to deposit :");
-					depositAmount = s.nextFloat();
+					depositAmount = s.nextDouble();
 					balance += depositAmount;
 					break;
 				case 2:
 					System.out.print("Enter the amount you want to Withdraw :");
-					withdrawAmount = s.nextFloat();
-					if (withdrawAmount > balance)
-						System.out.println("Can not withdraw amount more than the balance");
-					else
+					withdrawAmount = s.nextDouble();
+					if (withdrawAmount > balance) {
+						System.out.println("Insufficient funds! Your balance is: " + balance);
+					} else if (withdrawAmount > 0) {
 						balance -= withdrawAmount;
+						System.out.println("Withdrawal successful! Remaining Balance: " + balance);
+					} else {
+						System.out.println("Invalid withdrawal amount.");
+					}
 					break;
 				case 3:
 					System.out.println("Your Balance is : " + balance);
 					break;
 				case 4:
+					if (PhoneNo == "")
+						System.out.println("There is no Registered Phone no.");
+					else
+						System.out.println("Your Phone no is : " + PhoneNo);
+					break;
+				case 5:
+					System.out.print("Enter your new phone number: ");
+					PhoneNo = s.next();
+					System.out.println("Phone number updated successfully!");
+					break;
+				case 6:
 					System.out.println("Exit");
 					break;
 				default:
@@ -52,7 +74,7 @@ class Bank {
 					break;
 			}
 
-		} while (choice != 4);
+		} while (choice != 6);
 
 		s.close();
 
--- Without oop Concept/SortedArray.java
+++ Without oop Concept/SortedArray.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class SortedArray {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int n, num;
+        int n, num, largestElem;
         int arr[] = new int[20];
         System.out.print("Enter the size of array : ");
         n = s.nextInt();
@@ -13,7 +13,6 @@ public class SortedArray {
             num = s.nextInt();
             arr[i] = num;
         }
-        s.close();
 
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n + 1; j++) {
@@ -24,10 +23,16 @@ public class SortedArray {
                 }
             }
         }
-
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
-
+        System.out.print("\nEnter the rank of the largest element you want to see (e.g., 1 for largest): ");
+        largestElem = s.nextInt();
+        if (largestElem > 0 && largestElem <= n)
+            System.out.println(arr[--largestElem]);
+        else
+            System.out.println("Invalid rank. Please enter a value between 1 and " + n);
+        s.close();
     }
+
 }
