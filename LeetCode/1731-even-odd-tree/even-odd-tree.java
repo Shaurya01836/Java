ad402160c0fe1010eabd6db5276d48e80e0ebba9
Shaurya Upadhyay <153098108+Shaurya01836@users.noreply.github.com>
2026-07-02 12:59:51 +0530

    Time: 22 ms (6.44%) | Memory: 74.3 MB (94.79%) - LeetSync

---
+++ LeetCode/1731-even-odd-tree/even-odd-tree.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        int level = 0;
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = queue.remove();
+
+                list.add(temp.val);
+
+                if (temp.left != null)
+                    queue.add(temp.left);
+                if (temp.right != null)
+                    queue.add(temp.right);
+
+                int s = list.size()-1 ; 
+
+                if (level % 2 == 0) {
+                        if ((list.size() > 1 && list.get(s) <= list.get(s - 1)) || list.get(s) % 2 == 0)
+                            return false;
+                } else {
+                        if ((list.size() > 1 && list.get(s) >= list.get(s - 1)) || list.get(s) % 2 != 0)
+                            return false;
+                }
+            }
+
+            level++;
+        }
+
+        return true;
+    }
+}
