4a3609c9c752b929c93b142d91985475a26fddbf
Shaurya Upadhyay <153098108+Shaurya01836@users.noreply.github.com>
2026-07-18 22:45:48 +0530

    Time: 5 ms (80.70%) | Memory: 46 MB (34.16%) - LeetSync

---
+++ LeetCode/9-palindrome-number/palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        int num = x;
+        int reversed = 0;
+
+        while (num > 0) {
+            reversed = (reversed * 10 )+ num % 10;
+            num /= 10 ; 
+        }
+
+        return reversed == x ; 
+    }
+}
