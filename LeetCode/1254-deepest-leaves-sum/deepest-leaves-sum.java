323b653f4895bbcfaedba22bc0e210ddef53e9fd
Shaurya Upadhyay <153098108+Shaurya01836@users.noreply.github.com>
2026-07-27 15:41:08 +0530

    Time: 2 ms (81.71%) | Memory: 48.4 MB (34.81%) - LeetSync

--- LeetCode/1254-deepest-leaves-sum/deepest-leaves-sum.java
+++ LeetCode/1254-deepest-leaves-sum/deepest-leaves-sum.java
@@ -14,34 +14,33 @@
  * }
  */
 class Solution {
+
+    int depth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int left = depth(root.left);
+        int right = depth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+    int res = 0;
+
+    void traverse(TreeNode root, int maxDepth, int currDepth) {
+        if (root == null)
+            return;
+
+        if (root.left == null && root.right == null && maxDepth == currDepth) {
+            res += root.val;
+        }
+
+        traverse(root.left, maxDepth, currDepth + 1);
+        traverse(root.right, maxDepth, currDepth + 1);
+    }
+
     public int deepestLeavesSum(TreeNode root) {
-
-        Queue<TreeNode> q = new LinkedList<>();
-        int res = 0;
-
-        q.add(root);
-
-        List<Integer> list = new ArrayList<>();
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-            list = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode curr = q.remove();
-
-                list.add(curr.val);
-
-                if (curr.left != null)
-                    q.add(curr.left);
-                if (curr.right != null)
-                    q.add(curr.right);
-            }
-        }
-
-        for (int i = 0; i < list.size(); i++) {
-            res += list.get(i);
-        }
+        traverse(root, depth(root), 1);
 
         return res;
     }
