57012cf45282cf33364057732f497556799b0b14
Shaurya Upadhyay <153098108+Shaurya01836@users.noreply.github.com>
2026-07-31 15:07:39 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (34.51%) - LeetSync

--- LeetCode/1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
+++ LeetCode/1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -1,7 +1,5 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        Stack<Integer> stack = new Stack<>();
-
         List<String> res = new ArrayList<>();
 
         int j = 0;
@@ -10,11 +8,9 @@ class Solution {
 
             if (j < target.length) {
                 res.add("Push");
-                stack.push(i);
             }
 
             if (j < target.length && target[j] != i) {
-                stack.pop();
                 res.add("Pop");
             } else {
                 j++;
